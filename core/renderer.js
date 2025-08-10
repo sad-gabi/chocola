e38901ef9e84ac8d72d03b99e699cf9afc792935
Sad Gabi <sadgabi20@gmail.com>
2025-08-10 13:39:55 -0600

    Update renderer.js

--- core/renderer.js
+++ core/renderer.js
@@ -2,9 +2,14 @@ import { renderComponents } from './componentLoader.js';
 
 export async function renderView(viewModule) {
   const view = (await viewModule()).default();
+
   document.title = view.title;
   let html = view.html;
   html = renderComponents(html);
+
   document.querySelector('#app').innerHTML = html;
-  for (const script of view.scripts || []) await script();
+
+  for (const script of view.scripts || []) {
+    await script();
+  }
 }
